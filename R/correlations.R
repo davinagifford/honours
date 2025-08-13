bea7e215ae11695b244609d5901131005e70deb2
Davina Gifford <davinagifford@gmail.com>
2025-08-13 14:20:59 +1000

    Correlations

--- R/correlations.R
+++ R/correlations.R
@@ -8,23 +8,27 @@ residuals_north <- resid(lm_fit_north)
 
 residuals_south <- resid(lm_fit_south)
 
-# Correlate the residuals
-residuals_correlation <- cor(residuals_north, residuals_south, method = "pearson")
-residuals_cor_test <- cor.test(residuals_north, residuals_south, method = "pearson")
+# Compare means and variances
+mean(residuals_north); mean(residuals_south)
+var(residuals_north); var(residuals_south)
 
-# Output results
-print(paste("Pearson correlation of residuals:", round(residuals_correlation, 3)))
-print(residuals_cor_test)
+# Plot distributions
+hist(residuals_north, col="blue", main="Residual North", xlab="Residual Value")
+hist(residuals_south, col="red", main="Residual South", xlab="Residual Value")
+
 
 # Get fitted values from both models
 fitted_north <- fitted(lm_fit_north)   # Predicted North from South
 fitted_south <- fitted(lm_fit_south) # Predicted South from North
 
 
-# Run Pearson correlation
-fitted_correlation <- cor(fitted_south, fitted_north, method = "pearson")
-fitted_cor_test <- cor.test(fitted_south, fitted_north, method = "pearson")
+# Compare means and variances
+mean(fitted_north); mean(fitted_south)
+var(fitted_north); var(fitted_south)
 
-# Output results
-print(paste("Pearson correlation of fitted values:", round(fitted_correlation, 3)))
-print(fitted_cor_test)
+# Plot distributions
+hist(fitted_north, col="blue", main="Fitted North", xlab="Fitted Value")
+hist(fitted_south, col="red", main="Fitted South", xlab="Fitted Value")
+
+
+boxplot(fitted_north, fitted_south, names = c("North", "South"), main = "Fitted Value Comparison")
